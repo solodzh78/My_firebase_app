0b9f5c0be424496eb79b1b2f8dd471ea9b759f5f
Sergei Solodzhuk <solodzh@gmail.com>
2021-09-20 19:25:57 +0200

    Первый commit в dev

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.[chunkhash].js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true,
+  },
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ],
+}
